efda94f3c0ff6c13fc871137ab7a5dcf728b0377
YasaShrestha <yasashrestha2010@gmail.com>
2023-10-08 12:22:34 +0100

    Initial Setup and GUI Created for the Assignment.

---
+++ GUI_Program/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace GUI_Program
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            richTextBox1 = new RichTextBox();
+            button1 = new Button();
+            button2 = new Button();
+            panel1 = new Panel();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(1, 17);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(472, 295);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(12, 402);
+            button1.Name = "button1";
+            button1.Size = new Size(153, 48);
+            button1.TabIndex = 1;
+            button1.Text = "Run";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.MenuText;
+            button2.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.ForeColor = SystemColors.Control;
+            button2.Location = new Point(248, 402);
+            button2.Name = "button2";
+            button2.Size = new Size(151, 48);
+            button2.TabIndex = 2;
+            button2.Text = "Syntax";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Location = new Point(511, 17);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(428, 295);
+            panel1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(1, 339);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(472, 27);
+            textBox1.TabIndex = 4;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(951, 753);
+            Controls.Add(textBox1);
+            Controls.Add(panel1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(richTextBox1);
+            Name = "Form1";
+            Text = "Graphical Interface Program";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+        private Button button1;
+        private Button button2;
+        private Panel panel1;
+        private TextBox textBox1;
+    }
+}
---
+++ GUI_Program/Form1.cs
@@ -0,0 +1,30 @@
+namespace GUI_Program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Click button Run");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Click on Syntax");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
