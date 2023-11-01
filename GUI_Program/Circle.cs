01c40214f352a0c6ce24f532f5e077d21a79f813
YasaShrestha <yasashrestha2010@gmail.com>
2023-11-01 11:48:19 +0000

    work on circle commmand

---
+++ GUI_Program/Circle.cs
@@ -0,0 +1,22 @@
+﻿namespace Shapes
+{
+    // Parent class
+    public class Circle : Shape
+    {
+        protected int radius;
+        
+
+        public Circle(Graphics graphics, Pen pen,int xpos,int ypos, int radius) : base(graphics, pen, xpos, ypos)
+        {
+            this.graphics = graphics;
+            this.pen = pen;
+            this.radius = radius;
+        }
+
+        public override void Draw()
+        {
+            graphics.DrawEllipse(pen, Xpos - radius, Ypos - radius, 2 * radius, 2 * radius);
+        }
+    }
+}
+
