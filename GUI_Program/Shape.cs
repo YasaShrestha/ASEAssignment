ce54279fa85bfec8761e7ecf8a0815faf3aa4132
YasaShrestha <yasashrestha2010@gmail.com>
2023-11-01 11:47:53 +0000

    draw base class created

---
+++ GUI_Program/Shape.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Shapes
+{ 
+    // Parent class
+    public abstract class Shape
+{
+    protected Graphics graphics;
+    protected Pen pen;
+    protected int Xpos;
+    protected int Ypos;
+
+    public Shape(Graphics graphics, Pen pen, int xpos, int ypos)
+        {
+            this.graphics = graphics;
+            this.pen = pen;
+            this.Xpos = xpos;
+            this.Ypos = ypos;
+        }
+
+        public abstract void Draw();
+
+}
+}
+
