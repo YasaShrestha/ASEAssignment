42e10c088fb56ff5f3200b5b0c4df897465c4710
YasaShrestha <yasashrestha2010@gmail.com>
2023-11-01 11:49:28 +0000

    Worked on Rectangle command.

---
+++ GUI_Program/Rectangle.cs
@@ -0,0 +1,23 @@
+﻿namespace Shapes
+{
+    // Parent class
+    public class Rectangle : Shape
+    {
+        protected int width;
+        protected int height;
+
+        public Rectangle(Graphics graphics, Pen pen,int xpos,int ypos, int width, int height) : base(graphics,pen, xpos,ypos)
+        {
+            this.graphics = graphics;
+            this.pen = pen;
+            this.width = width;
+            this.height = height;
+        }
+
+        public override void Draw()
+        {
+            graphics.DrawRectangle(pen, Xpos, Ypos, width, height);
+        }
+    }
+}
+    
