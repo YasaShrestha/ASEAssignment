c7cd935d71a0a3cf30289f4ea45614bc7caef043
YasaShrestha <yasashrestha2010@gmail.com>
2023-11-01 11:50:03 +0000

    Worked on Triangle command

---
+++ GUI_Program/Triangle.cs
@@ -0,0 +1,29 @@
+﻿namespace Shapes
+{
+    // Parent class
+    public class Triangle : Shape
+    {
+        protected int width;
+        protected int height;
+
+        public Triangle(Graphics graphics, Pen pen, int xpos, int ypos, int width, int height) : base(graphics, pen, xpos, ypos)
+        {
+            this.graphics = graphics;
+            this.pen = pen;
+            this.width = width;
+            this.height = height;
+        }
+
+        public override void Draw()
+        {
+            Point[] trianglePoints = {
+            new Point(Xpos, Ypos), // Top point
+            new Point(Xpos, height+Ypos),    // Bottom-left point
+            new Point(height+Ypos,width+Xpos) // Bottom-right point
+            };
+
+            graphics.DrawPolygon(pen, trianglePoints);
+        }
+    }
+}
+
