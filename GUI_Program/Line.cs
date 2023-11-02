4c8f1b5c2b22f8bd890e3349af582acb9f5a7051
YasaShrestha <yasashrestha2010@gmail.com>
2023-11-02 16:07:28 +0000

    Created Line Class

---
+++ GUI_Program/Line.cs
@@ -0,0 +1,31 @@
+﻿namespace Shapes
+{
+   
+    public class Line : Shape
+    {
+        protected int startPoint;
+        protected int endPoint;
+
+
+        public Line(Graphics graphics, Pen pen, int xpos, int ypos,int startPoint, int endPoint) : base(graphics, pen, xpos, ypos)
+        {
+            this.graphics = graphics;
+            this.pen = pen;
+            this.endPoint = endPoint;
+            this.startPoint = startPoint;
+        }
+
+        public override void Draw()
+        {
+
+            // Create points that define line.
+            PointF point1 = new PointF(this.Xpos, this.Ypos);
+            PointF point2 = new PointF(startPoint, endPoint);
+
+            // Draw line to screen.
+            graphics.DrawLine(pen, point1, point2);
+            
+        }
+    }
+}
+
