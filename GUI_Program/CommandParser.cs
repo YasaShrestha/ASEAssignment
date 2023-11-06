255382cbd2b84ae4652957ef242c2be970810693
YasaShrestha <yasashrestha2010@gmail.com>
2023-11-06 13:48:25 +0000

    Worked on command parser class and changed the if case into switch case.

--- GUI_Program/CommandParser.cs
+++ GUI_Program/CommandParser.cs
@@ -7,126 +7,164 @@ using System.Threading.Tasks;
 
 namespace GUI_Program
 {
+    using System;
+    using System.Drawing;
+    /*Command Parser class to controll all the commands given by the user.*/
     public class CommandParser
     {
         private Color PenColor = Color.Black;
-
         private int Xpos = 0;
         private int Ypos = 0;
 
-        public void CommandParserProcess(Graphics g, String inputCommand) {
-            //google string split on the basis of  new line 
-            String[] lineByLineCmdReader = inputCommand.Split("\n");
-            for (int i = 0; i < lineByLineCmdReader.Length; i++)
+        public void CommandParserProcess(Graphics g, String inputCommand)
+        {
+            if (inputCommand == null || inputCommand.Length == 0)
             {
-                Pen pen = new Pen(PenColor);
+                MessageBox.Show("Command not found. Please input command.");
+                return;
+            }
+                // Split input commands by line
+                String[] lineByLineCmdReader = inputCommand.Split("\n");
 
-                var cmd = lineByLineCmdReader[i];
-                var splitted = cmd.Split(' ', 2);
-                var inputCommandPartOnly = splitted[0].ToLower().Trim();
-
-                var inputParam = "";
-                if (splitted.Length > 1)
+                for (int i = 0; i < lineByLineCmdReader.Length; i++)
                 {
-                    inputParam = splitted[1];
-                }
+                    Pen pen = new Pen(PenColor);
 
+                    var cmd = lineByLineCmdReader[i];
+                    var splitted = cmd.Split(' ', 2);
+                    var inputCommandPartOnly = splitted[0].ToLower().Trim();
 
-                String[] paramArray = inputParam.Split(",");
+                    var inputParam = "";
+                    if (splitted.Length > 1)
+                    {
+                        inputParam = splitted[1];
+                    }
 
-                if (inputCommandPartOnly.ToLower().Equals("circle") || inputCommandPartOnly.ToLower().Equals("circle"))
-                {
-                    int raduis = Int32.Parse(paramArray[0]); ;
-                    Shape shape = new Shapes.Circle(g, pen, Xpos, Ypos, raduis);
-                    shape.Draw();
-                }
-                else if (inputCommandPartOnly.Equals("rectangle") || inputCommandPartOnly.Equals("rectangle"))
-                {
+                    String[] paramArray = inputParam.Split(",");
 
+                    // Process different commands based on the inputCommandPartOnly
+                    switch (inputCommandPartOnly)
+                    {
+                        case "circle":
+                        if (paramArray ==null||paramArray.Length!= 1 || paramArray[0].Trim().Length==0|| !int.TryParse(paramArray[0], out _))
+                        {
+                            MessageBox.Show("Cirlce need one parameter. Please input the radius value.");
+                            return;
+                        }
+                   
+                        
+                            int radius = Int32.Parse(paramArray[0]);
 
-                    int width = Int32.Parse(paramArray[0]);
-                    int height = Int32.Parse(paramArray[1]);
-                    Shape shape = new Shapes.Rectangle(g, pen, Xpos, Ypos, width, height);
-                    shape.Draw();
+                            Shape circle = new Shapes.Circle(g, pen, Xpos, Ypos, radius);
+                            circle.Draw();
+                            break;
 
-                }
-                else if (inputCommandPartOnly.Equals("drawto") || inputCommandPartOnly.Equals("drawto"))
-                {
-                    int startingPoint = Int32.Parse(paramArray[0]);
-                    int endPoint = Int32.Parse(paramArray[1]);
-                    Shape shape = new Shapes.Line(g, pen, Xpos, Ypos, startingPoint, endPoint);
-                    shape.Draw();
-                }
-                else if (inputCommandPartOnly.Equals("trangle") || inputCommandPartOnly.Equals("trangle"))
-                {
+                        case "rectangle":
+                        if (paramArray.Length != 2 || !int.TryParse(paramArray[0], out _)|| !int.TryParse(paramArray[1], out _))
+                        {
+                            MessageBox.Show("Please input valid width and height for rectangle ");
+                            return;
 
-                    int width = Int32.Parse(paramArray[0]);
-                    int height = Int32.Parse(paramArray[1]);
-                    Shape shape = new Shapes.Triangle(g, pen, Xpos, Ypos, width, height);
-                    shape.Draw();
-                }
-                else if (inputCommandPartOnly.Equals("clear") || inputCommandPartOnly.Equals("clear"))
-                {
-                    clearDraw(g);
-                }
-                else if (inputCommandPartOnly.Equals("reset") || inputCommandPartOnly.Equals("reset"))
-                {
+                        }
+                        int width = Int32.Parse(paramArray[0]);
+                            int height = Int32.Parse(paramArray[1]);
+                            Shape rectangle = new Shapes.Rectangle(g, pen, Xpos, Ypos, width, height);
+                            rectangle.Draw();
+                            break;
 
-                    reset(g);
-                }
-                else if (inputCommandPartOnly.Equals("moveto") || inputCommandPartOnly.Equals("moveto"))
-                {
-                    int Xpos = Int32.Parse(paramArray[0]);
-                    int Ypos = Int32.Parse(paramArray[1]);
-                    moveTo(Xpos, Ypos);
-                }
-                else if (inputCommandPartOnly.Equals("pen") || inputCommandPartOnly.Equals("pen"))
-                {
+                        case "drawto":
+                        if (paramArray.Length != 2 || !int.TryParse(paramArray[0], out _) && !int.TryParse(paramArray[1], out _))
+                        {
+                            MessageBox.Show("Please input valid starting point value and end point value ");
+                            return;
 
-                    var colorVal = paramArray[0];
+                        }
+                        int startingPoint = Int32.Parse(paramArray[0]);
+                            int endPoint = Int32.Parse(paramArray[1]);
+                            Shape line = new Shapes.Line(g, pen, Xpos, Ypos, startingPoint, endPoint);
+                            line.Draw();
+                            break;
 
+                        case "triangle":
+                        if (paramArray.Length != 2 || !int.TryParse(paramArray[0], out _) || !int.TryParse(paramArray[1], out _))
+                        {
+                            MessageBox.Show("Please input valid width and height for the triangle ");
+                            return;
 
-                    PenColor = Color.FromName(colorVal);
-                }
-                //code block end
-                else
-                {
+                        }
+                        int triWidth = Int32.Parse(paramArray[0]);
+                            int triHeight = Int32.Parse(paramArray[1]);
+                            Shape triangle = new Shapes.Triangle(g, pen, Xpos, Ypos, triWidth, triHeight);
+                            triangle.Draw();
+                            break;
 
-                    MessageBox.Show("Command not supported.");
+                        case "clear":
+                            clearDraw(g);
+                            break;
+
+                        case "reset":
+                            reset(g);
+                            break;
+
+                        case "moveto":
+                        if (paramArray.Length != 2 || !int.TryParse(paramArray[0], out _) || !int.TryParse(paramArray[1], out _))
+                        {
+                            MessageBox.Show("Please input valid X axis and Y axis ");
+                            return;
+
+                        }
+                        int newXpos = Int32.Parse(paramArray[0]);
+                            int newYpos = Int32.Parse(paramArray[1]);
+                            moveTo(newXpos, newYpos);
+                            break;
+
+                        case "pen":
+                        if ( !(paramArray[0] is string)&&paramArray[0] != "red"&& paramArray[0] != "green" && paramArray[0]!="blue")
+                        {
+                            MessageBox.Show("Please input valid colour :red green or blue");
+
+                             return;
+                         }
+                            PenColor = Color.FromName(paramArray[0]);
+                        
+
+                            break;
+
+                        default:
+                            // If the command is not recognized
+                            MessageBox.Show("Command not supported.");
+                            break;
+                    }
                 }
             }
-        
-        }
+            
 
+        /* Method to clear all the drawings */
         private void clearDraw(Graphics g)
         {
             g.Clear(Color.SkyBlue);
         }
 
+        /* Method to reset graphics and move pen point to 0,0 */
         private void reset(Graphics g)
         {
-            // Create pen.
             Pen pen = new Pen(PenColor);
 
             pen.ResetTransform();
             pen.Dispose();
-            //this.moveTo(0,0);
             this.Xpos = 0;
             this.Ypos = 0;
             this.clearDraw(g);
-
         }
 
-        private void moveTo(int Xpos, int Ypos)
+        /* Method to move the X,Y points in the drawing board */
+        private void moveTo(int newXpos, int newYpos)
         {
-            this.Xpos = Xpos;
-            this.Ypos = Ypos;
-
-
+            this.Xpos = newXpos;
+            this.Ypos = newYpos;
         }
-
-
     }
+
 
 
 }
