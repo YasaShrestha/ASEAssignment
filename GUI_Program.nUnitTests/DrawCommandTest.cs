cf9556b3334aca06670b5ecf5e79f871d5577e6b
YasaShrestha <yasashrestha2010@gmail.com>
2024-01-05 17:01:47 +0000

    working on test implementation

---
+++ GUI_Program.nUnitTests/DrawCommandTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_Program.nUnitTests
+{
+    internal class DrawCommandTest
+    {
+
+        CommandParser parser;
+        [SetUp]
+        public void Setup()
+        {
+            parser = new CommandParser();
+        }
+
+        [Test]
+        public void CircleValidTest()
+        {
+            String command = "circle 80"; ;
+            parser.processEngine(null, command, true);
+
+            Assert.AreEqual("0,0", parser.getPenPos());
+        }
+
+        [Test]
+        public void RectangleValidTest()
+        {
+            String command = "rectangle 40,40"; ;
+            parser.processEngine(null, command, true);
+
+            Assert.AreEqual("0,0", parser.getPenPos());
+        }
+    }
+}
---
+++ GUI_Program.nUnitTests/InvalidDrawCommandTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_Program.nUnitTests
+{
+    internal class InvalidDrawCommandTest
+    {
+
+        CommandParser parser;
+        [SetUp]
+        public void Setup()
+        {
+            parser = new CommandParser();
+        }
+
+        [Test]
+        public void CircleInvalidTest()
+        {
+            String command = "circle x"; ;
+            
+
+            var exception = Assert.Throws<GPLException>(() => parser.processEngine(null, command, true));
+
+            Assert.AreEqual("Cirlce need one parameter. Please input the valid radius value. Error at Line 1", exception.Message);
+            Assert.AreEqual("0,0", parser.getPenPos());
+        }
+
+        [Test]
+        public void RectangleInvalidTest()
+        {
+            String command = "rectangle"; ;
+
+
+            var exception = Assert.Throws<GPLException>(() => parser.processEngine(null, command, true));
+
+            Assert.AreEqual("Please input valid width and height for rectangle  Error at Line 1", exception.Message);
+            Assert.AreEqual("0,0", parser.getPenPos());
+        }
+    }
+}
