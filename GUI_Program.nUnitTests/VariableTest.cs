a343f636cf5f3ef3b479348e9308e84f3d5a8fe5
YasaShrestha <yasashrestha2010@gmail.com>
2024-01-06 22:49:55 +0000

    Implemented test case for variable

---
+++ GUI_Program.nUnitTests/VariableTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_Program.nUnitTests
+{
+    internal class VariableTest
+    {
+
+        CommandParser parser;
+        [SetUp]
+        public void Setup()
+        {
+            parser = new CommandParser();
+        }
+
+
+        [Test]
+        public void VariableValidTest()
+        {
+            String command = "a=10";
+            parser.processEngine(null, command, true);
+
+            Assert.AreEqual("0,0", parser.getPenPos());
+        }
+
+        [Test]
+        public void VariableValidTest2()
+        {
+            String command = "a=10\r\na=a*10";
+            parser.processEngine(null, command, true);
+
+            Assert.AreEqual("0,0", parser.getPenPos());
+        }
+    }
+}
