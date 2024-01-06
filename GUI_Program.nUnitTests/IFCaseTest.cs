0695ef9d43d3add31247232a204dda473f2e4da8
YasaShrestha <yasashrestha2010@gmail.com>
2024-01-06 22:49:03 +0000

    Implemented If loop test case

--- GUI_Program.nUnitTests/IFCaseTest.cs
+++ GUI_Program.nUnitTests/IFCaseTest.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace GUI_Program.nUnitTests
 {
-    /* 
+    /* this is a test class for valid  and invalid test of IF command.
      */
     internal class IfCaseTest
     {
@@ -22,14 +22,43 @@ namespace GUI_Program.nUnitTests
         [Test]
         public void IfValidTest()
         {
-            String command = "if 20"; ;
+            String command = "if 5<20"; 
             parser.processEngine(null, command, true);
 
             Assert.AreEqual("0,0", parser.getPenPos());
         }
 
-     
+        [Test]
+        public void IfValidTest2()
+        {
+            String command = "if 70>50"; 
+            parser.processEngine(null, command, true);
+            Assert.AreEqual("0,0", parser.getPenPos());
+        }
 
-       
+        [Test]
+        public void IfInValidTest()
+        {
+            String command = "if a<"; 
+            var exception = Assert.Throws<GPLException>(() => parser.processEngine(null, command, true));
+
+            Assert.AreEqual("a value is not a integer.", exception.Message);
+
+            Assert.AreEqual("0,0", parser.getPenPos());
+        }
+
+        [Test]
+        public void IfInValidTest2()
+        {
+            String command = "if <=b"; 
+            var exception = Assert.Throws<GPLException>(() => parser.processEngine(null, command, true));
+
+            Assert.AreEqual(" value is not a integer.", exception.Message);
+            Assert.AreEqual("0,0", parser.getPenPos());
+        }
+
+
+
+
     }
 }
