442093eadd1f05e2592453515549466cc93b9f6c
YasaShrestha <yasashrestha2010@gmail.com>
2024-01-07 13:02:34 +0000

    Implemented test case for methods

---
+++ GUI_Program.nUnitTests/MethodTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_Program.nUnitTests
+{
+    internal class MethodTest
+    {
+
+        CommandParser parser;
+        [SetUp]
+        public void Setup()
+        {
+            parser = new CommandParser();
+        }
+
+
+        [Test]
+        public void MethodValidTest()
+        {
+            String command = "method run\r\ncircle 10\r\nendmethod";
+            parser.processEngine(null, command, true);
+
+            Assert.AreEqual("0,0", parser.getPenPos());
+
+        }
+            [Test]
+            public void MethodValidTest2()
+            {
+                String command = "method run\r\ncircle 50\r\nendmethod\r\ncallmethod run";
+                parser.processEngine(null, command, true);
+
+                Assert.AreEqual("0,0", parser.getPenPos());
+            }
+
+
+        }
+    }
